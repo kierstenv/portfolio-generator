647867fc748c31e9e84192bbf8e6ef3a25c9ea02
kierstenv <kierstenvicknair22@gmail.com>
2022-03-09 10:40:02 -0600

    make HTML template + generate page from user input

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
